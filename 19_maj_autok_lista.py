9c2c25d45709ddeb44f3c8e80c41dfe997428aa5
Ákos Klobetz <klobetz@gmail.com>
2022-07-06 11:32:55 +0200

    #1 2. feladat

--- 19_maj_autok_lista.py
+++ 19_maj_autok_lista.py
@@ -12,5 +12,11 @@ with open("TXT_allomanyok/autok.txt", encoding="utf8") as f:
         adatok = [nap,ido,rendszam,azonosito,km,kibehajtas]
         autoklista.append(adatok)
 
+#2. Adja meg, hogy melyik autót vitték el utoljára a parkolóból! Az eredményt a mintának megfelelően írja a képernyőre!
 
+for elem in autoklista:
+    if elem[5] == 0:
+        nap = elem[0]
+        rendszam = elem[2]
 
+print(f"2. feladat\n{nap} nap rendszám: {rendszam} ")
