b5391307291e3880004638bfac10d4dd904617cd
Ákos Klobetz <klobetz@gmail.com>
2022-08-24 14:57:56 +0200

    #1 3.feladat

--- 18_maj_fogadoora_lista.py
+++ 18_maj_fogadoora_lista.py
@@ -15,4 +15,18 @@ with open("TXT_allomanyok/fogado.txt", encoding="utf8") as f:
 print(f"2. feladat\nFoglalások száma: {len(fogadooralista)}")
 
 #3. feladat:
+#valasztanar = input("3. feladat\nAdjon meg egy nevet: ")
+valasztanar = "Nagy Ferenc"
+#valasztanar = "Farkas Attila"
+db = 0
+for tanar,foglaltido,foglalasdatuma in fogadooralista:
+    if tanar == valasztanar:
+        db += 1
+
+if db > 0 :
+    print(f"{valasztanar} néven {db} időpontfoglalás van.")
+else:
+    print("A megadott néven nincs időpontfoglalás.")
+
+
 
